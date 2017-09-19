5a00b2d6d87d68c590d790a4912ad01b141cbe31
joshgressman <joshgressman@gmail.com>
2017-09-19 17:47:46 -0500

    added trending gifs

--- src/app/trending/trending.component.ts
+++ src/app/trending/trending.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { GiphyService } from '../giphy.service';
 
 @Component({
   selector: 'app-trending',
@@ -6,10 +7,39 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./trending.component.css']
 })
 export class TrendingComponent implements OnInit {
+   private offset = 0;
+   private perPage = 12;
+   public results: any;
+   public gifs: Array<any> = [];
+   public isLoading: boolean =  true;
+  //  offset and perPage will be used to manage pagination
+  // results will be used to store the response from the server
 
-  constructor() { }
+  constructor(private giphyService: GiphyService) { }
+
+   getTrendingGifs(offset, limit){
+     this.giphyService.getTrendingGifs(offset, limit).subscribe(
+       (data) => {
+         this.results = data;
+         this.gifs = this.gifs.concat(this.results.data);
+         this.isLoading = false;
+       },
+       (err) => console.log("Opps,", err),
+       () => console.log('Response', this.results)
+     )
+   }
+
+   getMore() {
+     this.isLoading = true;
+     this.offset = this.offset + this.perPage;
+     this.getTrendingGifs(this.offset, this.perPage);
+   }
+
 
   ngOnInit() {
+    this.getTrendingGifs(this.offset, this.perPage);
   }
+
+
 
 }
